b39fb53403a67526789395288437abb9b4fc5f10
Ömer <omer@Omer-MacBook-Air-2.local>
2026-03-06 16:54:24 +0300

    completed all exercises

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    // Aynı isimden var mı kontrol et (Egzersiz 2.7)
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={(e) => setNewName(e.target.value)} /></div>
+        <div>number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
+        <div><button type="submit">add</button></div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <p key={person.id}>{person.name} {person.number}</p>
+      )}
+    </div>
+  )
+}
+
+export default App
